a10d95d2bb85e789595d66ca3c444f753b6eb7ee
sourcebrain <sourcebrain@gmail.com>
2015-08-23 17:53:15 +0530

    Navigation Drawer added.
    Navigation drawer added and it i general for all the application.

--- IndianSoccerLeague2015/app/src/main/java/main/isl/sb/com/indiansoccerleague2015/MainActivity.java
+++ IndianSoccerLeague2015/app/src/main/java/main/isl/sb/com/indiansoccerleague2015/MainActivity.java
@@ -1,6 +1,8 @@
 package main.isl.sb.com.indiansoccerleague2015;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,13 +11,18 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-private Toolbar toolbar;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar =(Toolbar)findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
@@ -37,7 +44,7 @@ private Toolbar toolbar;
             return true;
         }
         if (id == R.id.navigation) {
-            startActivity(new Intent(this,FixtureActivity.class));
+            startActivity(new Intent(this, FixtureActivity.class));
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ IndianSoccerLeague2015/app/src/main/java/main/isl/sb/com/indiansoccerleague2015/NavigationDrawerFragment.java
@@ -0,0 +1,98 @@
+package main.isl.sb.com.indiansoccerleague2015;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NavigationDrawerFragment extends Fragment {
+
+    private static final String PREF_FILE_NAME = "pref_file";
+    private static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+   // private static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
+    private ActionBarDrawerToggle mDrawerToogle;
+    private DrawerLayout mDrawerLayout;
+    private boolean mUserLearnedDrawer;
+    private boolean mSavedInstanceState;
+    private View containerView;
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUserLearnedDrawer =Boolean.valueOf(readFromPreferences(getActivity(),KEY_USER_LEARNED_DRAWER,"false"));
+        if(savedInstanceState!=null){
+            mSavedInstanceState = true;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+    }
+
+
+    public void setUp(int fragmentId,DrawerLayout drawerLayout, Toolbar toolbar) {
+        containerView = getActivity().findViewById(fragmentId);
+        mDrawerLayout = drawerLayout;
+        mDrawerToogle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar,
+                R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                if(!mUserLearnedDrawer){
+                    mUserLearnedDrawer =true;
+                    saveToPreferences(getActivity(),KEY_USER_LEARNED_DRAWER,mUserLearnedDrawer+"");
+                }
+                getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getActivity().invalidateOptionsMenu();
+            }
+        };
+        if(!mUserLearnedDrawer&&!mSavedInstanceState){
+            mDrawerLayout.openDrawer(containerView);
+        }
+        mDrawerLayout.setDrawerListener(mDrawerToogle);
+        mDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mDrawerToogle.syncState();
+            }
+        });
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName,
+                                         String preferenceValue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,
+                context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+    public static String readFromPreferences(Context context, String preferenceName,
+                                         String defaultvalue) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME,
+                context.MODE_PRIVATE);
+        return sharedPreferences.getString(preferenceName,defaultvalue);
+    }
+}
